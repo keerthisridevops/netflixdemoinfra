706764e79a4ddedd743dbc3d82c6916c1bb4c2b5
keerthisridevops <keerthishree511@gmail.com>
2025-02-03 12:24:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "rahull"
+key_name = "projectkeypair"
 vpc_security_group_ids = ["sg-0a44a666528734bfb"]
 tags = {
 Name = var.instance_names[count.index]
